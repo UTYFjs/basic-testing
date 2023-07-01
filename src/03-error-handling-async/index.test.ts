6440f6d9966c176ff57399f6b67f9744e3d6d54b
UTYFjs <utyf-90@mail.ru>
2023-07-01 20:35:47 +0300

    feat: add 03 error handling async tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -9,28 +9,36 @@ import {
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const value = 'awesome value';
+    expect(resolveValue(value)).resolves.toBe(value);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    const mess = 'Awesome provided throw';
+    expect(() => {
+      throwError(mess);
+    }).toThrow(mess);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect(() => {
+      throwError();
+    }).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect(() => {
+      throwCustomError();
+    }).toThrow(new MyAwesomeError());
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    expect(rejectCustomError()).rejects.toThrow(new MyAwesomeError());
   });
 });
