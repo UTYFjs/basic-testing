c99f9ebc88fe218099f961ff93bbac92d54edcda
Henadzi Suhakou <gennadiy.sugakov@gmail.com>
2025-05-07 21:12:48 +0300

    feat: add test cases for simple test and table test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,37 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 1, b: 2, action: Action.Subtract, expected: -1 },
+  { a: 13, b: 2, action: Action.Subtract, expected: 11 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
+  { a: 13, b: 5, action: Action.Multiply, expected: 65 },
+  { a: 3, b: 2, action: Action.Divide, expected: 1.5 },
+  { a: 12, b: 2, action: Action.Divide, expected: 6 },
+  { a: -15, b: 5, action: Action.Divide, expected: -3 },
+  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 10, b: 3, action: Action.Exponentiate, expected: 1000 },
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: 3, b: 2, action: undefined, expected: null },
+  { a: 10, b: 3, action: '~', expected: null },
+  { a: 3, b: 3, action: '--', expected: null },
+  { a: '3', b: 2, action: Action.Exponentiate, expected: null },
+  { a: null, b: 3, action: Action.Divide, expected: null },
+  { a: 3, b: '3', action: Action.Multiply, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('table tests', ({ a, b, action, expected }) => {
+    const result = simpleCalculator({
+      a: a,
+      b: b,
+      action: action,
+    });
+    expect(result).toEqual(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
